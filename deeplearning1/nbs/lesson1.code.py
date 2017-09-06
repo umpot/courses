621a209d0c368a1f77eac24838711ccd96d6abe4
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-09-06 12:36:10 +0300

    m

--- deeplearning1/nbs/lesson1.code.py
+++ deeplearning1/nbs/lesson1.code.py
@@ -26,7 +26,7 @@ from vgg16 import Vgg16
 vgg = Vgg16()
 # Grab a few images at a time for training and validation.
 # NB: They must be in subdirectories named based on their category
-batches = vgg.get_batches(path+'train', batch_size=batch_size)
-val_batches = vgg.get_batches(path+'valid', batch_size=batch_size*2)
+batches = vgg.get_batches(path+'/train', batch_size=batch_size)
+val_batches = vgg.get_batches(path+'/valid', batch_size=batch_size*2)
 vgg.finetune(batches)
 vgg.fit(batches, val_batches, nb_epoch=1)
