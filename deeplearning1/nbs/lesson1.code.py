02e92e16dbed02b9fb0515b14813e680823f325c
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-09-06 12:05:34 +0300

    m

---
+++ deeplearning1/nbs/lesson1.code.py
@@ -0,0 +1,29 @@
+from keras import backend
+backend.set_image_dim_ordering('th')
+
+path = "../../data/dogscats/"
+
+from __future__ import division,print_function
+
+import os, json
+from glob import glob
+import numpy as np
+np.set_printoptions(precision=4, linewidth=100)
+from matplotlib import pyplot as plt
+print(os.getcwd())
+
+import utils; reload(utils)
+from utils import plots
+
+batch_size=64
+
+import vgg16; reload(vgg16)
+from vgg16 import Vgg16
+
+vgg = Vgg16()
+# Grab a few images at a time for training and validation.
+# NB: They must be in subdirectories named based on their category
+batches = vgg.get_batches(path+'train', batch_size=batch_size)
+val_batches = vgg.get_batches(path+'valid', batch_size=batch_size*2)
+vgg.finetune(batches)
+vgg.fit(batches, val_batches, nb_epoch=1)
